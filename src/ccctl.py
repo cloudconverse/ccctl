a94c320c833f26ad2a4ab2ae5f52ad2a44478a28
Neeran Gul <neerangul@MITL0161.local>
2024-09-22 00:19:49 +0100

    add this

--- src/ccctl.py
+++ src/ccctl.py
@@ -5,14 +5,16 @@ from config import CcctlConfig
 app = typer.Typer()
 
 @app.command()
-def init(integration: str, 
-         llm_endpoint: Annotated[Optional[str], typer.Argument()] = None,
-         model: Annotated[Optional[str], typer.Argument()] = None):
+def init_aws(integration: str, 
+             llm_endpoint: Annotated[Optional[str], typer.Argument()] = None,
+             model: Annotated[Optional[str], typer.Argument()] = None,
+             aws_sso_profile: Annotated[Optional[str], typer.Argument()] = None,
+             aws_regions: Annotated[Optional[str], typer.Argument()] = None):
     print(f"Hello {integration}")
-    ccctl_config = CcctlConfig(llm_endpoint, model)
+    ccctl_config = CcctlConfig(integration, llm_endpoint, model, aws_sso_profile, aws_regions)
 
     if integration == "aws":
-        aws_steampipe = 
+        aws_steampipe = AWSSteampipe
 
 @app.command()
 def query
--- src/config.py
+++ src/config.py
@@ -3,7 +3,8 @@ import configparser
 import os
 
 class CcctlConfig:
-	def __init__(self, llm_endpoint=None, model=None):
+	def __init__(self, integration, llm_endpoint=None, model=None):
+		self.integration = integration
 		self.llm_endpoint = llm_endpoint
 		self.model = model
 		self.config = configparser.ConfigParser()
@@ -11,8 +12,8 @@ class CcctlConfig:
 	def setup(self):
 		home = Path.home()
 		os.mkdir(f"{home}/.ccctl")
-		self.config["default"]["llm_endpoint"] = self.llm_endpoint
-		self.config["default"]["model"] = self.model 
+		self.config[self.integration]["llm_endpoint"] = self.llm_endpoint
+		self.config[self.integration]["model"] = self.model 
 		with open(f"{home}/ccctl/config", 'w') as configfile:
   			self.config.write(configfile)
 
