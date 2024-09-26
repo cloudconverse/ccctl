d5966cfcd4fe952e6cb5c77c814c98cfcb35d6cd
Neeran Gul <neerangul@MacBookPro.communityfibre.co.uk>
2024-09-26 22:08:31 +0100

    fix it

--- src/config.py
+++ src/config.py
@@ -1,28 +1,32 @@
 from pathlib import Path
-import configparser
 import os
+import yaml
 
 class CcctlConfig:
-	def __init__(self, integration, llm_endpoint=None, model=None, **kwargs):
-		self.integration = integration
-		self.llm_endpoint = llm_endpoint or ""
-		self.model = model or ""
-		self.aws_sso_profile = kwargs.get("aws_sso_profile", "")
-		self.aws_regions = kwargs.get("aws_regions", [])
-		self.config = configparser.ConfigParser()
-		self.config[self.integration] = {}
+    def __init__(self, integration, llm_endpoint=None, model=None, **kwargs):
+        self.integration = integration
+        self.llm_endpoint = llm_endpoint or ""
+        self.model = model or ""
+        self.aws_sso_profile = kwargs.get("aws_sso_profile", "")
+        self.aws_regions = kwargs.get("aws_regions", [])
+        self.config = {}
+        self.config[self.integration] = {}
+        self.home = Path.home()
 
-	def setup_aws(self):
-		home = Path.home()
-		if not os.path.isdir(f"{home}/.ccctl"):
-			os.mkdir(f"{home}/.ccctl")
-		self.config[self.integration]["llm_endpoint"] = self.llm_endpoint
-		self.config[self.integration]["model"] = self.model
+    def load_aws(self):
+        with open(f"{self.home}/.ccctl/config.yaml",'r') as f:
+            return yaml.safe_load(f)
 
-		if self.aws_sso_profile:
-			self.config[self.integration]["aws_sso_profile"] = self.aws_sso_profile
-			self.config[self.integration]["aws_regions"] = self.aws_regions
+    def setup_aws(self):
+        if not os.path.isdir(f"{self.home}/.ccctl"):
+            os.mkdir(f"{self.home}/.ccctl")
+        self.config[self.integration]["llm_endpoint"] = self.llm_endpoint
+        self.config[self.integration]["model"] = self.model
 
-		with open(f"{home}/.ccctl/config", 'w+') as configfile:
-  			self.config.write(configfile)
+        if self.aws_sso_profile:
+            self.config[self.integration]["aws_sso_profile"] = self.aws_sso_profile
+            self.config[self.integration]["aws_regions"] = self.aws_regions
+
+        with open(f"{self.home}/.ccctl/config.yaml", 'w+') as configfile:
+            yaml.dump(self.config, configfile, sort_keys=False) 
 
